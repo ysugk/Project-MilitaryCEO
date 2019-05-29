b44e774cc742219ad2fefcc91757e0d92ce81f00
Yongseok <ysugk@snu.ac.kr>
2019-05-30 01:15:55 +0900

    Participants

--- build/code/collect_code.R
+++ build/code/collect_code.R
@@ -128,7 +128,7 @@ get_totalCount <- function(ntatComptDateBegin, ntatComptDateEnd) {
   json$response$body$totalCount
 }
 
-bypage_crawl <- function (p, ntatComptDateBegin, ntatComptDateEnd) {
+bypage_crawl <- function(p, ntatComptDateBegin, ntatComptDateEnd) {
   Sys.sleep(1)
   
   pageNo <- glue("pageNo=", p)
@@ -194,4 +194,138 @@ walk(1:length(url_seq), byurl_crawl)
 df <- dir_ls("build/temp/FacilityNegoDetail") %>%
   map_dfr(read_csv)
 
+## Facility Competition
+base_url <- "http://openapi.d2b.go.kr/openapi/service/BidResultInfoService/"
+operation <- "getFcltyCmpetBidResultList?"
 
+date_seq <- seq.Date(from = ymd(20060101),
+                     to = ymd(20190101),
+                     by = "1 year")
+
+begin_seq <- date_seq[2:length(date_seq) - 1] %>% str_remove_all("-")
+end_seq <- (date_seq[2:length(date_seq)] - ddays(1)) %>% str_remove_all("-")
+
+get_totalCount <- function(opengDateBegin, opengDateEnd) {
+  json <- glue(base_url, operation, opengDateBegin, "&", opengDateEnd) %>%
+    jsonlite::fromJSON()
+  
+  json$response$body$totalCount
+}
+
+bypage_crawl <- function(p, opengDateBegin, opengDateEnd) {
+  Sys.sleep(1)
+  
+  pageNo <- glue("pageNo=", p)
+  numOfRows <- "numOfRows=10000"
+  
+  option <- glue(opengDateBegin, opengDateEnd, numOfRows, pageNo, .sep = "&")
+  url <- glue(base_url, operation, option)
+  json <- jsonlite::fromJSON(url)
+  
+  json$response$body$items %>%
+    first() %>%
+    mutate_all(as.character)
+}
+
+byperiod_crawl <- function(begin, end) {
+  print(glue("Processing ", begin, "--", end))
+  
+  opengDateBegin <- glue("opengDateBegin=", begin)
+  opengDateEnd <- glue("opengDateEnd=", end)
+  
+  totalCount <- get_totalCount(opengDateBegin, opengDateEnd)
+  totalPage <- ceiling(totalCount/10000)
+  
+  df <- map_dfr(1:totalPage, bypage_crawl, opengDateBegin, opengDateEnd)
+  write_csv(df, glue("build/temp/FacilityCompetitionResult/", begin, "_", end, ".csv"))
+}
+
+walk2(begin_seq, end_seq, byperiod_crawl)
+df <- vroom::vroom(fs::dir_ls("build/temp/FacilityCompetitionResult/"))
+vroom::vroom_write(df, "build/output/facility_cmptresult.csv")
+
+## Facility Competition Detail
+facility_cmptresult <- read_delim("build/output/facility_cmptresult.csv", delim = "\t")
+operation <- "getFcltyCmpetBidResultDetail?"
+
+cntrwkNo <- paste0("cntrwkNo=", facility_cmptresult$cntrwkNo)
+opengDate <- paste0("opengDate=", facility_cmptresult$opengDate)
+orntCode <- paste0("orntCode=", facility_cmptresult$orntCode)
+pblancNo <- paste0("pblancNo=", facility_cmptresult$pblancNo)
+pblancOdr <- paste0("pblancOdr=", facility_cmptresult$pblancOdr)
+option <- paste(cntrwkNo, opengDate, orntCode, pblancNo, pblancOdr,  sep = "&")
+
+url_seq <- paste0(base_url, operation, option)
+
+byurl_crawl <- function(i) {
+  
+  print(glue("Processing ", url_seq[i]))
+  Sys.sleep(0.1)
+  url <- url_seq[i]
+  
+  json <- jsonlite::fromJSON(url)
+  
+  if (json$response$body == "") {
+    return(NULL)
+  }
+  
+  df <- json$response$body$item %>%
+    bind_cols()
+  
+  write_csv(df, glue("build/temp/FacilityCompetitionDetail/", i, ".csv"))
+}
+
+walk(1:length(url_seq), byurl_crawl)
+df <- fs::dir_ls("build/temp/FacilityCompetitionDetail") %>%
+  map_dfr(read_csv, col_types = rep("c", 21) %>% paste0(collapse = ""))
+write_excel_csv(df, "build/output/facility_cmptdetail.csv")
+
+## Facility Competition Participants
+facility_cmptresult <- read_delim("build/output/facility_cmptresult.csv", delim = "\t")
+operation <- "getFcltyCmpetBidResultMnufList?"
+
+orntCode <- paste0("orntCode=", facility_cmptresult$orntCode)
+cntrwkNo <- paste0("cntrwkNo=", facility_cmptresult$cntrwkNo)
+ntatPlanDate <- paste0("ntatPlanDate=", facility_cmptresult$opengDate)
+option <- paste(cntrwkNo, orntCode, ntatPlanDate, sep = "&")
+
+url_seq <- paste0(base_url, operation, option)
+
+byurl_crawl <- function(i) {
+  
+  print(glue("Processing ", url_seq[i]))
+  Sys.sleep(0.1)
+  url <- url_seq[i]
+  
+  json <- jsonlite::fromJSON(url)
+  
+  if (json$response$body == "") {
+    return(NULL)
+  }
+  
+  df <- json$response$body$item %>%
+    bind_cols()
+  
+  write_csv(df, glue("build/temp/FacilityCompetitionParticipant/", i, ".csv"))
+}
+
+str_extract(path_seq[1000], "\\d+(?=.csv)")
+
+walk(1:length(url_seq), byurl_crawl)
+path_seq <- fs::dir_ls("build/temp/FacilityCompetitionParticipant")
+df <- map_dfr(path_seq, function(path){
+  read_csv(path,
+           col_types = rep("c", 13) %>% paste0(collapse = "")) %>%
+    mutate(num = str_extract(path, "\\d+(?=.csv)"))
+    })
+
+df %>%
+  distinct(bznsRgnb, mfkrName) %>%
+  drop_na(bznsRgnb) %>%
+  write_excel_csv("build/output/facility_cmptprtcpt_code.csv")
+
+facility_cmptresult %>%
+  rowid_to_column() %>%
+  mutate(rowid = as.character(rowid)) %>%
+  right_join(df, by = c("rowid" = "num")) %>%
+  write_excel_csv("build/output/facility_cmptprtcpt.csv")
